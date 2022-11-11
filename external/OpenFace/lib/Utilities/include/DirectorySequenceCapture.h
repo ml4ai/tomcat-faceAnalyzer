6550b4ec02a0636673cdacb141579199356c7aa5
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2022-11-11 14:45:35 -0700

    Add files via upload

--- external/OpenFace/lib/Utilities/include/DirectorySequenceCapture.h
+++ external/OpenFace/lib/Utilities/include/DirectorySequenceCapture.h
@@ -1,33 +1,37 @@
 #pragma once
 
-#include <string>
+#include "boost/filesystem.hpp"
 #include <opencv2/highgui/highgui.hpp>
 
+using boost::filesystem::path;
+using boost::filesystem::directory_iterator;
+
 namespace Utilities {
+    
     //=======================================================================
     /**
     A class for capturing images using inotify from a NFS directory
         This program waits for an image to be written to the NFS directory and
         uses this image file to generate and return a cv::Mat object
     */
-    class InotifySequenceCapture {
+    class DirectorySequenceCapture {
 
         private:
-            std::string monitorPath;
-            int fd, watch_desc;
+            path p;
+            directory_iterator directory_itr;
+            directory_iterator end_itr;
+            std::string current_filename;
 
         public:
             // Default constructor
-            InotifySequenceCapture() = default;
-
-            // Initialize the inotify watch by passing it the path to the NFS directory
-            void initPath(const std::string &path);
+            DirectorySequenceCapture(const std::string &path);
 
             // Wait for the next image file to get written, and use this image file 
             // to return a Mat object
             cv::Mat GetNextFrame();
 
-            // Close the inotify watch on the given directory
-            void closeInotify();
+            std::string getCurrentFileName();
+
+            path getMonitoringPath();
     };
 }
