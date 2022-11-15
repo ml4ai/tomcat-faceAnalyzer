ecc4aa7545c1b78c08a5c8bddde97bfcb3938bbe
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2022-11-14 18:05:50 -0700

    Filename as timestamp during post-processing (#10)
    * use filename as timestamp
    
    * file modification time
    
    * fix compile error
    
    * fix compile
    
    * fix timestamp not in file

--- external/OpenFace/lib/Utilities/src/DirectorySequenceCapture.cpp
+++ external/OpenFace/lib/Utilities/src/DirectorySequenceCapture.cpp
@@ -1,5 +1,9 @@
 #include "DirectorySequenceCapture.h"
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <sys/stat.h>
+#include <iomanip>
 
 using namespace Utilities;
 
@@ -8,25 +12,51 @@ DirectorySequenceCapture::DirectorySequenceCapture(const std::string &path):
 
 cv::Mat DirectorySequenceCapture::GetNextFrame() {
     while (this->directory_itr != this->end_itr) {
-        this->current_filename = this->directory_itr->path().string();
+        this->current_filepath = this->directory_itr->path().string();
+        this->current_filename = this->directory_itr->path().stem().string();
         this->directory_itr++;
 
-        if (this->current_filename.substr(this->current_filename.find_last_of(".") + 1) == "png") {
-            auto readImage = cv::imread(this->current_filename);
-
+        if (this->current_filepath.find("png") != std::string::npos &&
+            this->current_filepath.find("out") == std::string::npos) {
+            auto readImage = cv::imread(this->current_filepath);
             if (!readImage.empty())
                 return readImage;
         }
 
-        std::cout << "Skipped " << this->current_filename << std::endl;
+        std::cout << "Skipped " << this->current_filepath << std::endl;
     }
 
     this->current_filename = "";
+    this->current_filepath = "";
     return cv::Mat();
 }
 
 std::string DirectorySequenceCapture::getCurrentFileName() {
     return this->current_filename;
+}
+
+std::string DirectorySequenceCapture::getCurrentFilePath() {
+    return this->current_filepath;
+}
+
+std::string DirectorySequenceCapture::getFileTimestampFromName() {
+    std::string timestamp_string = this->current_filename;
+    std::replace( timestamp_string.begin(), timestamp_string.end(), '_', ':');
+    return timestamp_string;
+}
+
+std::string DirectorySequenceCapture::getFileModifiedTimestamp() {
+    struct stat t_stat;
+    stat(this->getCurrentFilePath().c_str(), &t_stat);
+    struct tm *time_now_tm = gmtime(&t_stat.st_mtime);
+
+    char timestampBuffer[300];
+    strftime(timestampBuffer, sizeof(timestampBuffer), "%FT%T", time_now_tm);
+
+    std::stringstream timestampStream;
+    timestampStream << timestampBuffer << '.' << std::setfill('0') << std::setw(9) << t_stat.st_mtim.tv_nsec << 'Z';
+
+    return timestampStream.str();
 }
 
 path DirectorySequenceCapture::getMonitoringPath() {
